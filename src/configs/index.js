a2879b7896c8f3cda8f6f090c7b3397871dd66bb
Alice-babayalo <umuhozawasealice@gmail.com>
2024-04-18 12:53:40 +0200

    first tral

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const Configs = {
-    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING,
+    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING||"mongodb+srv://Alice:12345@cluster0.6eqmb7j.mongodb.net/cloneSheCanCode",
     CLIENT_APP: process.env.CLIENT_APP || 'http://localhost:5173',
-    PORT: process.env.PORT,
+    PORT: process.env.PORT||3000,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
     JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN,
